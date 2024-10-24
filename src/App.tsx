dbbfa8ac987b2d6f152481db8a258aa592d566a5
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-24 22:33:35 +0900

    feat: 라우터 돔 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   );
 };
 
