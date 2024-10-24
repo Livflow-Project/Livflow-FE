ba03173d73dd07a08aaf0ff8a4a82c38ee076243
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-24 22:18:25 +0900

    design: 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+      main: '#121212',
+      caption: '#333333',
+      primary: '#004aad',
+      primary_hover: '#003e92',
+      button: '#336ebd',
+      button_hover: '#004aad',
+      m_background: '#d9d9d9',
+      underline: '#333333'
+    },},
   },
   plugins: [],
 }
