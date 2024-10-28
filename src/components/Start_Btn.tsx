d944b0c2bee16577a12adbfe5297259174766eab
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-28 15:24:05 +0900

    design: css 수정

--- src/components/Start_Btn.tsx
+++ src/components/Start_Btn.tsx
@@ -3,17 +3,17 @@ import R_Arrow from '../assets/R_Arrow.svg';
 
 const Start_Btn = () => {
   return (
-    <div className='relative w-fit'>
+    <div className='fixed bottom-5 flex w-[100%] justify-center'>
       <Link to='/login'>
         <button className='bg-button hover:bg-button_hover relative h-[62px] w-[274px] rounded-[96px] text-xl text-white shadow-md'>
           Livflow 시작하기
+          <img
+            src={R_Arrow}
+            alt='Arrow Icon'
+            className='absolute right-8 top-1/2 -translate-y-1/2 transform'
+          />
         </button>
       </Link>
-      <img
-        src={R_Arrow}
-        alt='Arrow Icon'
-        className='absolute right-8 top-1/2 -translate-y-1/2 transform'
-      />
     </div>
   );
 };
