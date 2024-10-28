f32f11e6849daf0c2d3ea1d63c3f0cfc28c7a5f2
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-28 21:08:31 +0900

    feat: 로그인 페이지 디자인 완료

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,43 @@
+import Google from '../assets/Google.svg';
+import Kakao from '../assets/Kakao.svg';
+import { Link } from 'react-router-dom';
+import Logo from '/public/Logo.svg';
+import Naver from '../assets/Naver.svg';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='flex h-svh w-[100%] flex-col items-center justify-center gap-[55px]'>
+      <Link to='/'>
+        <img src={Logo} alt='Livflow 로고 이미지' className='w-[500px]' />
+      </Link>
+      <div className='flex flex-col items-center justify-center gap-5'>
+        <button
+          className='text-main h-[60px] w-[620px] text-xl font-medium'
+          style={{
+            backgroundImage: `url(${Kakao})`,
+          }}
+        >
+          카카오 로그인
+        </button>
+        <button
+          className='h-[60px] w-[620px] text-xl font-medium text-white'
+          style={{
+            backgroundImage: `url(${Naver})`,
+          }}
+        >
+          네이버 로그인
+        </button>
+        <button
+          className='text-main h-[60px] w-[620px] text-xl font-medium text-opacity-50 shadow-md'
+          style={{
+            backgroundImage: `url(${Google})`,
+          }}
+        >
+          구글 로그인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
