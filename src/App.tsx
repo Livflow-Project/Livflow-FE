40fa04485468ee2e4093ab9dbbdd76e9ca9c9e48
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-28 15:23:21 +0900

    feat: 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+      </Route>
+
       <Route path='/login' element={<Login />} />
     </Routes>
   );
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Footer from './Footer';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
