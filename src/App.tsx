f9a4cea94000071457aef96b48df2e20642d373b
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-29 15:38:39 +0900

    feat: 스토어 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
+import MyStore from './pages/MyStore';
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/mystore' element={<MyStore />} />
       </Route>
 
       <Route path='/login' element={<Login />} />
