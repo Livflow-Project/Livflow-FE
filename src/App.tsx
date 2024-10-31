056e683c1d9d3cbe71c6b457a564636104cb1b31
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-31 22:31:39 +0900

    feat:  토스트 임포트

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
 import Store from './pages/Store';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-      </Route>
+    <>
+      <ToastContainer />
 
-      <Route path='/login' element={<Login />} />
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+        </Route>
+
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
