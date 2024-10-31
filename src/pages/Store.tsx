539881f0b9d0321b3f89ba0fa8a7317295382b11
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-31 22:32:14 +0900

    feat: 스토어 사용

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,16 +1,25 @@
 import Add_Store from '../components/Add_Store';
 import My_Store from '../components/My_Store';
+import useUsers_Store from '../Store/useUsers_Store';
 
 const Store = () => {
+  const { stores } = useUsers_Store();
+
   return (
     <div className='flex h-[calc(100vh-75px)] flex-col items-center justify-center bg-white'>
       <div className='w-full max-w-[1116px]'>
         <div className='text-caption mb-10 text-3xl font-semibold'>
-          전체 스토어 (0)
+          전체 스토어 ({stores.length})
         </div>
         <div className='flex items-center gap-12'>
-          <My_Store />
-          <My_Store />
+          {stores.map((store, index) => (
+            <My_Store
+              key={index}
+              id={store.id}
+              name={store.name}
+              address={store.address}
+            />
+          ))}
           <Add_Store />
         </div>
       </div>
