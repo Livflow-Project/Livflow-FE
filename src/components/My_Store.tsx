1d013bd611bd0e5f53de5ed7236efe8f86632b95
Yi-HyeonJu <guswwn1925@gmail.com>
2024-10-31 22:34:51 +0900

    feat: 이름, 주소 수정 및 자동 포커스 기능 추가

--- src/components/My_Store.tsx
+++ src/components/My_Store.tsx
@@ -1,28 +1,112 @@
+import { useRef, useState } from 'react';
+
 import Map_Icon from '../assets/Map_Icon.svg';
 import PieChart from './PieChart';
 import Store_Icon from '../assets/Store_Icon.svg';
+import useUsers_Store from '../Store/useUsers_Store';
 
-const My_Store = () => {
+interface MyStoreProps {
+  id: number;
+  name: string;
+  address?: string;
+}
+
+const My_Store: React.FC<MyStoreProps> = ({
+  id,
+  name: initialName,
+  address: initialAddress,
+}) => {
+  const [name, setName] = useState(initialName);
+  const [address, setAddress] = useState(initialAddress);
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingAddress, setIsEditingAddress] = useState(false);
+
+  const updateStore = useUsers_Store((state) => state.updateStore);
+
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const addressInputRef = useRef<HTMLInputElement>(null);
+
+  const handleUpdate = (type: 'name' | 'address') => {
+    const updatedData = { id, name, address };
+
+    if (type === 'name' && name !== initialName) {
+      updateStore(updatedData);
+    } else if (type === 'address' && address !== initialAddress) {
+      updateStore(updatedData);
+    }
+
+    // 편집 모드 종료
+    setIsEditingName(false);
+    setIsEditingAddress(false);
+  };
+
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    type: 'name' | 'address'
+  ) => {
+    if (e.key === 'Enter') {
+      handleUpdate(type);
+    }
+  };
+
+  const handleEditButtonClick = (type: 'name' | 'address') => {
+    if (type === 'name') {
+      setIsEditingName(true);
+      setTimeout(() => nameInputRef.current?.focus(), 0);
+    } else if (type === 'address') {
+      setIsEditingAddress(true);
+      setTimeout(() => addressInputRef.current?.focus(), 0);
+    }
+  };
+
   return (
     <div className='store_box'>
       <div className='border-underline border-b border-opacity-20 p-[20px]'>
         <ul className='flex flex-col gap-6'>
           <li className='flex items-center justify-between'>
             <img src={Store_Icon} alt='상점 아이콘' className='w-[30px]' />
-            <span className='text-main text-[17px] font-semibold'>
-              GOD 내린 커피
-            </span>
-            <button className='text-primary hover:text-primary_hover text-[13px]'>
-              수정
+            {isEditingName ? (
+              <input
+                ref={nameInputRef}
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                onKeyDown={(e) => handleKeyDown(e, 'name')}
+                className='text-main border-b border-gray-300 text-center text-[17px] font-semibold outline-none'
+              />
+            ) : (
+              <span className='text-main w-[200px] truncate text-center text-[17px] font-semibold'>
+                {name}
+              </span>
+            )}
+            <button
+              className='text-button/50 hover:text-button_hover text-[13px] outline-none hover:font-bold'
+              onClick={() => handleEditButtonClick('name')}
+            >
+              {isEditingName ? '완료' : '수정'}
             </button>
           </li>
           <li className='flex items-center justify-between'>
-            <img src={Map_Icon} alt='상점 아이콘' className='w-[30px]' />
-            <span className='text-main w-[220px] truncate text-center text-[17px] font-semibold'>
-              서울 동작구 123번지 56, 1층
-            </span>
-            <button className='text-primary hover:text-primary_hover text-[13px]'>
-              수정
+            <img src={Map_Icon} alt='주소 아이콘' className='w-[30px]' />
+            {isEditingAddress ? (
+              <input
+                ref={addressInputRef}
+                type='text'
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                onKeyDown={(e) => handleKeyDown(e, 'address')}
+                className='text-main border-b border-gray-300 text-center text-[17px] font-semibold outline-none'
+              />
+            ) : (
+              <span className='text-main w-[200px] truncate text-center text-[17px] font-semibold'>
+                {address}
+              </span>
+            )}
+            <button
+              className='text-button/50 hover:text-button_hover text-[13px] outline-none hover:font-bold'
+              onClick={() => handleEditButtonClick('address')}
+            >
+              {isEditingAddress ? '완료' : '수정'}
             </button>
           </li>
         </ul>
@@ -39,9 +123,6 @@ const My_Store = () => {
             수입
           </button>
         </div>
-        {/* <div className='text-caption text-xl font-medium'>
-          입력된 지출이 없습니다.
-        </div> */}
         <div className='h-full max-h-[270px]'>
           <PieChart />
         </div>
