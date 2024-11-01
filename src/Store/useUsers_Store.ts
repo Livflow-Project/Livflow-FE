050c6c53ea6c8ef22e9d5228536069ae24f1c58c
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-01 16:57:11 +0900

    feat: 삭제 기능 추가

--- src/Store/useUsers_Store.ts
+++ src/Store/useUsers_Store.ts
@@ -1,33 +1,59 @@
-// src/stores/storeStore.ts
 import { create } from 'zustand';
 
 interface Store {
-  id: number; // 각 상점을 고유하게 식별하기 위한 ID 추가
+  id: number;
   name: string;
   address?: string;
 }
 
 interface StoreState {
   stores: Store[];
+  isDeleteMode: boolean;
   addStore: (store: Omit<Store, 'id'>) => void;
   updateStore: (updatedStore: Store) => void;
+  deleteStore: (id: number) => void;
+  toggleDeleteMode: () => void;
 }
 
 const useUsers_Store = create<StoreState>((set) => ({
   stores: [],
+  isDeleteMode: false,
+
   addStore: (store) =>
     set((state) => {
       const newId =
         state.stores.length > 0
           ? Math.max(...state.stores.map((s) => s.id)) + 1
-          : 1; // 새로운 id 계산
-      return { stores: [...state.stores, { ...store, id: newId }] };
+          : 1;
+
+      return {
+        stores: [...state.stores, { ...store, id: newId }],
+        isDeleteMode: false,
+      };
     }),
+
   updateStore: (updatedStore) =>
     set((state) => ({
       stores: state.stores.map((store) =>
         store.id === updatedStore.id ? updatedStore : store
       ),
+    })),
+
+  deleteStore: (id) =>
+    set((state) => {
+      const updatedStores = state.stores.filter((store) => store.id !== id);
+      const isDeleteMode =
+        updatedStores.length > 0 ? state.isDeleteMode : false;
+
+      return {
+        stores: updatedStores,
+        isDeleteMode,
+      };
+    }),
+
+  toggleDeleteMode: () =>
+    set((state) => ({
+      isDeleteMode: state.stores.length > 0 ? !state.isDeleteMode : false,
     })),
 }));
 
