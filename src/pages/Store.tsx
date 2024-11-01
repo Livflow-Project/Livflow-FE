7f230c65b9cf273c420c4d9cf459c99816861167
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-01 16:56:07 +0900

    feat: 삭제 버튼 추가

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,26 +1,44 @@
 import Add_Store from '../components/Add_Store';
 import My_Store from '../components/My_Store';
+import { useState } from 'react';
 import useUsers_Store from '../Store/useUsers_Store';
 
 const Store = () => {
-  const { stores } = useUsers_Store();
+  const { stores, isDeleteMode, toggleDeleteMode } = useUsers_Store();
+
+  const handleDeleteModeToggle = () => {
+    toggleDeleteMode();
+  };
 
   return (
     <div className='flex h-[calc(100vh-75px)] flex-col items-center justify-center bg-white'>
-      <div className='w-full max-w-[1116px]'>
-        <div className='text-caption mb-10 text-3xl font-semibold'>
-          전체 스토어 ({stores.length})
+      <div className='max-w-[1116px]'>
+        <div className='mb-10 flex items-end justify-between'>
+          <span className='text-caption text-3xl font-semibold'>
+            전체 스토어 ({stores.length})
+          </span>
+          {stores.length !== 0 ? (
+            <button
+              className='text-red text-lg'
+              onClick={handleDeleteModeToggle}
+            >
+              {isDeleteMode ? '취소하기' : '삭제하기'}
+            </button>
+          ) : (
+            ''
+          )}
         </div>
         <div className='flex items-center gap-12'>
+          <Add_Store />
           {stores.map((store, index) => (
             <My_Store
               key={index}
               id={store.id}
               name={store.name}
               address={store.address}
+              isDeleteMode={isDeleteMode}
             />
           ))}
-          <Add_Store />
         </div>
       </div>
     </div>
