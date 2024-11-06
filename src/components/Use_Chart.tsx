7538fe08598c7830c41ad1838b6e7b800c896830
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-06 14:27:59 +0900

    remove: 삭제 버튼 분리

--- src/components/Use_Chart.tsx
+++ src/components/Use_Chart.tsx
@@ -1,31 +1,20 @@
 import PieChart from './PieChart';
 import { twMerge } from 'tailwind-merge';
 import { useState } from 'react';
-import useUsers_Store from '../Store/useUsers_Store';
 
 interface Use_ChartProps {
-  id: number;
   isDeleteMode: boolean;
 }
 
-const Use_Chart: React.FC<Use_ChartProps> = ({ id, isDeleteMode }) => {
+const Use_Chart: React.FC<Use_ChartProps> = ({ isDeleteMode }) => {
   const [isExpenseSelected, setIsExpenseSelected] = useState(true);
-
-  const deleteStore = useUsers_Store((state) => state.deleteStore);
-
-  const handleDelete = () => {
-    deleteStore(id);
-  };
 
   const handleToggle = (type: 'expense' | 'income') => {
     setIsExpenseSelected(type === 'expense');
   };
 
   return (
-    <div
-      className='flex flex-col items-center justify-between p-[20px]'
-      style={{ height: 'calc(100% - 128px)' }}
-    >
+    <>
       <div className='flex w-[100%] justify-between'>
         <button
           className={twMerge(
@@ -56,7 +45,7 @@ const Use_Chart: React.FC<Use_ChartProps> = ({ id, isDeleteMode }) => {
       </div>
       <div
         className={twMerge(
-          'text-caption max-h-[270px] text-xl font-medium',
+          'max-h-[270px] text-xl font-medium text-caption',
           isDeleteMode ? 'opacity-50' : ''
         )}
       >
@@ -65,13 +54,7 @@ const Use_Chart: React.FC<Use_ChartProps> = ({ id, isDeleteMode }) => {
           ? '입력된 지출이 없습니다.'
           : '입력된 수입이 없습니다.'} */}
       </div>
-      <button
-        className={twMerge(isDeleteMode ? 'delete_button' : 'choice_button')}
-        onClick={isDeleteMode ? handleDelete : undefined}
-      >
-        {isDeleteMode ? '삭제' : '선택'}
-      </button>
-    </div>
+    </>
   );
 };
 
