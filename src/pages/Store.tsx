162440b8db0b34c70ec3072b78908fa8c0b432de
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-06 14:27:41 +0900

    feat: 키값 변경, 스와이퍼 적용 시도 중

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,10 @@
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// import { Navigation, Pagination } from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
 import Add_Store from '../components/Add_Store';
 import My_Store from '../components/My_Store';
 import useUsers_Store from '../Store/useUsers_Store';
@@ -13,12 +20,12 @@ const Store = () => {
     <div className='flex h-[calc(100vh-75px)] flex-col items-center justify-center bg-white'>
       <div className='w-full max-w-[1116px]'>
         <div className='mb-10 flex w-full max-w-[340px] items-end justify-between'>
-          <span className='text-caption text-3xl font-semibold'>
+          <span className='text-3xl font-semibold text-caption'>
             전체 스토어 ({stores.length})
           </span>
           {stores.length !== 0 && (
             <button
-              className='text-red text-lg hover:font-semibold'
+              className='text-lg text-red hover:font-semibold'
               onClick={handleDeleteModeToggle}
             >
               {isDeleteMode ? '취소하기' : '삭제하기'}
@@ -27,9 +34,9 @@ const Store = () => {
         </div>
         <div className='flex items-start gap-12'>
           <Add_Store />
-          {stores.map((store, index) => (
+          {stores.map((store) => (
             <My_Store
-              key={index}
+              key={store.id}
               id={store.id}
               name={store.name}
               address={store.address}
