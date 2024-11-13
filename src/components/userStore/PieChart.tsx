4ee97f443754e3b4a03c9ba4ddff2d00ccc73d7f
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-13 18:13:58 +0900

    feat: 월별 정보 받아  타입별로 적용
    #14

--- src/components/userStore/PieChart.tsx
+++ src/components/userStore/PieChart.tsx
@@ -2,47 +2,38 @@ import Chart, { TooltipItem } from 'chart.js/auto';
 import { useEffect, useRef } from 'react';
 
 interface PieChartProps {
-  selectedType: 'expense' | 'income';
+  selectedType?: 'expense' | 'income';
+  categories: Record<string, number>;
 }
 
-const PieChart: React.FC<PieChartProps> = ({ selectedType }) => {
+const PieChart: React.FC<PieChartProps> = ({ selectedType, categories }) => {
   const chartRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     if (!chartRef.current) return;
 
     const ctx = chartRef.current.getContext('2d');
-
     if (!ctx) return;
 
-    const data =
-      selectedType === 'expense'
-        ? {
-            labels: ['생활용품', '간식', '식재료'],
-            datasets: [
-              {
-                label: ' ',
-                data: [3000, 5000, 1000],
-                backgroundColor: [
-                  'rgb(255, 99, 132)',
-                  'rgb(54, 162, 235)',
-                  'rgb(255, 205, 86)',
-                ],
-                hoverOffset: 5,
-              },
-            ],
-          }
-        : {
-            labels: ['알바', '앱태크'],
-            datasets: [
-              {
-                label: ' ',
-                data: [30000, 5000],
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
-                hoverOffset: 5,
-              },
-            ],
-          };
+    const labels = Object.keys(categories);
+    const dataValues = Object.values(categories);
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: selectedType === 'expense' ? '지출' : '수입',
+          data: dataValues,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+          ],
+          hoverOffset: 5,
+        },
+      ],
+    };
 
     const chartInstance = new Chart(ctx, {
       type: 'pie',
@@ -57,7 +48,7 @@ const PieChart: React.FC<PieChartProps> = ({ selectedType }) => {
             callbacks: {
               label: (tooltipItem: TooltipItem<'pie'>) => {
                 const value = tooltipItem.raw as number;
-                return ` : ${value.toLocaleString()}`;
+                return `${tooltipItem.label} : ${value.toLocaleString()}원`;
               },
             },
           },
@@ -68,7 +59,7 @@ const PieChart: React.FC<PieChartProps> = ({ selectedType }) => {
     return () => {
       chartInstance.destroy();
     };
-  }, [selectedType]);
+  }, [categories]);
 
   return <canvas ref={chartRef}></canvas>;
 };
