98131a730213c153a596f8840e8a2a7fa3a660df
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-13 18:14:28 +0900

    feat: 날짜 선택 CSS 적용 및 파이차트 추가 및 연동
    #14

--- src/components/calendar/Calender.tsx
+++ src/components/calendar/Calender.tsx
@@ -6,9 +6,11 @@ import { useRef, useState } from 'react';
 
 import CalendarModal from '../modal/CalendarModal';
 import FullCalendar from '@fullcalendar/react';
+import PieChart from '../userStore/PieChart';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import koLocale from '@fullcalendar/core/locales/ko';
 import listPlugin from '@fullcalendar/list';
+import { twMerge } from 'tailwind-merge';
 import useCalendarStore from '../../store/useCalendarStore';
 
 interface CalendarProps {
@@ -18,10 +20,17 @@ interface CalendarProps {
 const Calender: React.FC<CalendarProps> = ({ storeId }) => {
   const calendarRef = useRef<FullCalendar | null>(null);
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
-  const { transactions, addTransaction } = useCalendarStore();
+  const [selectedDateElement, setSelectedDateElement] =
+    useState<HTMLElement | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const { transactions, addTransaction } = useCalendarStore();
+  const { monthlyTotals } = useCalendarStore();
+
   const storeTransactions = transactions[storeId] || {};
+
+  const monthKey = new Date().toISOString().slice(0, 7);
+  const totalsForMonth = monthlyTotals[storeId]?.[monthKey];
 
   const events: CalendarEvent[] = Object.keys(storeTransactions).map(
     (date) => ({
@@ -29,13 +38,32 @@ const Calender: React.FC<CalendarProps> = ({ storeId }) => {
     })
   );
 
-  const handleDateClick = (info: DateClickArg) => {
-    setSelectedDate(info.dateStr);
-  };
-
   const handleAddTransaction = (transaction: Transaction) => {
     if (selectedDate) {
       addTransaction(storeId, selectedDate, transaction);
+    }
+  };
+
+  const handleDateClick = (info: DateClickArg) => {
+    // 이전에 선택된 날짜가 있으면 클래스 제거
+    if (selectedDateElement) {
+      selectedDateElement
+        .querySelector('.fc-daygrid-day-top')
+        ?.classList.remove('selected-date');
+    }
+
+    // 현재 클릭한 날짜가 이전에 선택된 날짜와 같으면 선택 해제
+    if (selectedDateElement === info.dayEl) {
+      setSelectedDateElement(null);
+      setSelectedDate(null);
+    } else {
+      // 새로운 날짜 선택 시 자식 요소에 클래스 추가
+      const dayTopElement = info.dayEl.querySelector('.fc-daygrid-day-top');
+      if (dayTopElement) {
+        dayTopElement.classList.add('selected-date');
+        setSelectedDateElement(info.dayEl);
+        setSelectedDate(info.dateStr);
+      }
     }
   };
 
@@ -104,53 +132,134 @@ const Calender: React.FC<CalendarProps> = ({ storeId }) => {
         />
       </div>
 
-      <div className='flex h-full w-[40%] flex-col items-center justify-between rounded-xl bg-white/50'>
-        {/* <div className='flex items-center justify-center h-full text-2xl text-main'>
-          <span>입력된 지출 / 수입이 없습니다.</span>
-        </div> */}
-        <div className='flex h-[55px] w-full items-center justify-around border-b border-underline'>
-          <span className='text-xl font-semibold text-main'>항목</span>
-          <span className='text-xl font-semibold text-main'>상세 정보</span>
-          <span className='text-xl font-semibold text-main'>지출 / 수입</span>
-        </div>
-
-        {/* 선택된 날짜의 트랜잭션 목록 표시 */}
-        <div className='flex flex-col'>
-          {selectedDate && storeTransactions[selectedDate]?.length > 0 ? (
-            storeTransactions[selectedDate].map((transaction, index) => (
-              <div key={index} className='flex justify-center py-2 w-fulls'>
-                <span>{transaction.item}</span>
-                <span>{transaction.details}</span>
-                <span
-                  className={
-                    transaction.type === 'expense' ? 'text-red' : 'text-green'
-                  }
-                >
-                  {transaction.type === 'expense'
-                    ? `-${transaction.amount}`
-                    : `+${transaction.amount}`}
-                </span>
-              </div>
-            ))
-          ) : (
-            <div className='text-2xl text-main'>
-              입력된 지출 / 수입이 없습니다.
-            </div>
-          )}
-        </div>
-
-        <div className='flex w-full items-center justify-between px-[25px] pb-[25px]'>
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className='soft_BcolorSet w-[230px]'
-          >
-            지출 / 수입 추가하기
-          </button>
-          <div className='flex gap-4'>
-            <button className='soft_BcolorSet w-[120px]'>수정 하기</button>
-            <button className='soft_BcolorSet w-[120px]'>삭제 하기</button>
+      <div className='flex h-full w-[40%] flex-col items-center rounded-xl bg-white/50'>
+        {/* 선택된 날짜가 없고 트랜잭션도 없을 때 */}
+        {!selectedDate && Object.keys(storeTransactions).length === 0 ? (
+          <div className='w-full my-auto text-2xl text-center text-main'>
+            <span>입력된 지출 / 수입이 없습니다.</span>
           </div>
-        </div>
+        ) : !selectedDate && Object.keys(storeTransactions).length > 0 ? (
+          // 선택된 날짜는 없지만 트랜잭션이 있을 때
+          <>
+            <div className='flex items-center w-full h-full justify-evenly'>
+              <div className='flex w-[40%] flex-col items-center justify-center'>
+                <span>총 지출</span>
+                <PieChart
+                  selectedType='expense'
+                  categories={totalsForMonth.categories.expense}
+                />
+                {/* 수입 카테고리들 중 가장큰 수입 최대 5순위까지 카테고리 */}
+                <ul>
+                  <li></li>
+                  <li>
+                    <span>합계</span>
+                    <span>- {totalsForMonth.expense.toLocaleString()}원</span>
+                  </li>
+                </ul>
+              </div>
+              <div className='h-[80%] w-[1px] bg-underline/50'></div>
+              <div className='flex w-[40%] flex-col items-center justify-center'>
+                <span>총 지출</span>
+                <PieChart
+                  selectedType='expense'
+                  categories={totalsForMonth.categories.income}
+                />
+                {/* 수입 카테고리들 중 가장큰 수입 최대 5순위까지 카테고리 */}
+                <ul>
+                  <li></li>
+                  <li>
+                    <span>합계</span>
+                    <span>+ {totalsForMonth.income.toLocaleString()}원</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* 지출 차트 */}
+            {/* <PieChart
+                selectedType='expense'
+                categories={totalsForMonth.categories.expense}
+              /> */}
+            {/* 수입 차트 */}
+            {/* <PieChart
+                selectedType='income'
+                categories={totalsForMonth.categories.income}
+              /> */}
+            {/* 총 합계 표시 */}
+            <div className='text-center'>
+              <p>
+                총 합계 :{' '}
+                {(
+                  totalsForMonth.income - totalsForMonth.expense
+                ).toLocaleString()}
+                원
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            {/* 선택된 날짜가 있거나 트랜잭션이 있을 때 */}
+            <div className='flex h-[65px] w-full items-center border-b border-underline'>
+              <span className='w-[30%] text-center text-xl font-semibold text-main'>
+                항목
+              </span>
+              <span className='w-[40%] text-center text-xl font-semibold text-main'>
+                상세 정보
+              </span>
+              <span className='w-[30%] text-center text-xl font-semibold text-main'>
+                지출 / 수입
+              </span>
+            </div>
+
+            <div className='flex flex-col w-full h-full gap-2'>
+              {selectedDate && storeTransactions[selectedDate]?.length > 0 ? (
+                storeTransactions[selectedDate].map((transaction, index) => (
+                  <div
+                    key={index}
+                    className='flex h-[45px] w-full items-center border-b border-underline/30 text-center'
+                  >
+                    <span className='w-[30%] text-lg font-normal'>
+                      {transaction.item}
+                    </span>
+                    <span className='w-[40%] text-lg font-normal'>
+                      {transaction.details}
+                    </span>
+                    <span
+                      className={twMerge(
+                        'w-[30%] text-lg font-normal',
+                        transaction.type === 'expense'
+                          ? 'text-red'
+                          : 'text-green'
+                      )}
+                    >
+                      {transaction.type === 'expense'
+                        ? `- ${transaction.amount}`
+                        : `+ ${transaction.amount}`}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                // 날짜는 선택했으나 트랜잭션이 없을 때 메시지 표시
+                <div className='w-full my-auto text-2xl text-center text-main'>
+                  입력된 지출 / 수입이 없습니다.
+                </div>
+              )}
+            </div>
+
+            <div className='flex w-full items-center justify-between px-[25px] pb-[20px]'>
+              <button
+                onClick={() => setIsModalOpen(true)}
+                className='soft_BcolorSet w-[40%]'
+              >
+                지출 / 수입 추가하기
+              </button>
+              <div className='flex w-[40%] gap-4'>
+                <button className='soft_BcolorSet w-[50%]'>수정 하기</button>
+                <button className='soft_BcolorSet w-[50%]'>삭제 하기</button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
 
       {/* 모달 컴포넌트 */}
