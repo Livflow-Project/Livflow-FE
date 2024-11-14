d61b39303638d948a1eb5bcb4456634209642430
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-14 21:50:43 +0900

    feat: 임시 랜딩페이지
    #17

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
-import Footer from '../components/layout/Footer';
+// import Footer from '../components/layout/Footer';
 import Start_Btn from '../components/Start_Btn';
 
 const Home = () => {
   return (
-    <>
-      <div className='bg-slate-600'>Home</div>
+    <div className='h- flex h-[calc(100vh-75px)] items-center justify-center'>
+      <div className='flex items-center gap-5'>
+        <img
+          src='../../public/LogoWhale.svg'
+          alt='메인 로고'
+          className='animate-smoothWaveAndSlide h-[200px]'
+        />
+        <img
+          src='../../public/LogoText.svg'
+          alt='메인 로고'
+          className='h-[70px]'
+        />
+      </div>
+
       <Start_Btn />
-      <Footer />
-    </>
+      {/* <Footer /> */}
+    </div>
   );
 };
 
