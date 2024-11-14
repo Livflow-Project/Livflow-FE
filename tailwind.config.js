e851d83e02699e7cacb7356669562e073b1a9849
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-14 21:50:26 +0900

    design: 애니메이션 추가
    #17

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,17 @@ export default {
       green: '#00C016',
       background: '#E4F0FF'
     },
+    animation: {
+      smoothWaveAndSlide: 'smoothWaveAndSlide 4s ease-in-out forwards', // 더 부드러운 이동을 위해 ease-in-out 사용
+      },
+      keyframes: {
+        smoothWaveAndSlide: {
+          '0%': { transform: 'translateX(-300%) translateY(0) rotate(0deg)' }, // 시작 위치
+          '33%': { transform: 'translateX(-160%) translateY(6px) rotate(3deg)' }, // 중간
+          '66%': { transform: 'translateX(-60%) translateY(-4px) rotate(-3deg)' }, // 끝에 가까운 위치
+          '100%': { transform: 'translateX(0) translateY(0) rotate(0deg)' }, // 최종 위치
+        },
+      },
     fontFamily: {
       sans: ['Pretendard','Roboto', 'sans-serif'],
     },
