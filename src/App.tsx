23757679635cfa2bd5b057c54c49907d5a495fa8
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-21 14:48:43 +0900

    fix: 경로 대소문자 구분

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Layout from './components/layout/Layout';
+import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
 import Store from './pages/Store';
 import StoreID from './pages/StoreID';
