3e1269d12ad736020bfabd81ecd4c9732055147f
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-22 17:13:20 +0900

    add: dist build 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // 빌드 결과물 디렉토리 (기본값: "dist")
+  },
+});
