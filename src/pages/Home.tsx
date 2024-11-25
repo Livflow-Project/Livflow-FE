6b928873638fe8742ac7409524f6620507202cef
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-25 18:25:46 +0900

    fix: public 경로 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,30 @@
+// // import Footer from '../components/layout/Footer';
+// import Start_Btn from '../components/Start_Btn';
+
+// const Home = () => {
+//   return (
+//     <div className='h- flex h-[calc(100vh-75px)] items-center justify-center'>
+//       <div className='flex items-center gap-5'>
+//         <img
+//           src='../../public/LogoWhale.svg'
+//           alt='메인 로고'
+//           className='animate-smoothWaveAndSlide h-[200px]'
+//         />
+//         <img
+//           src='../../public/LogoText.svg'
+//           alt='메인 로고'
+//           className='h-[70px]'
+//         />
+//       </div>
+
+//       <Start_Btn />
+//       {/* <Footer /> */}
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 // import Footer from '../components/layout/Footer';
 import Start_Btn from '../components/Start_Btn';
 
@@ -6,12 +33,12 @@ const Home = () => {
     <div className='h- flex h-[calc(100vh-75px)] items-center justify-center'>
       <div className='flex items-center gap-5'>
         <img
-          src='../../public/LogoWhale.svg'
+          src='/LogoWhale.svg'  // 루트 경로에서 이미지 참조
           alt='메인 로고'
           className='animate-smoothWaveAndSlide h-[200px]'
         />
         <img
-          src='../../public/LogoText.svg'
+          src='/LogoText.svg'  // 루트 경로에서 이미지 참조
           alt='메인 로고'
           className='h-[70px]'
         />
