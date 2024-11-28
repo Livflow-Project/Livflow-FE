5c61f88217d37b7865bcb7231b9e0bea5542eccc
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-29 05:06:35 +0900

    ✨ Feat: 추가 assets 파일에서 모든 svg 임포트
    #23

---
+++ src/assets/assets.ts
@@ -0,0 +1,23 @@
+import google from './login/google.svg';
+import kakao from './login/kakao.svg';
+import logo from './header/logo.svg';
+import logoText from './home/logoText.svg';
+import logoWhale from './home/logoWhale.svg';
+import mapIcon from './store/mapIcon.svg';
+import naver from './login/naver.svg';
+import plusButton from './store/plusButton.svg';
+import rightArrow from './home/rightArrow.svg';
+import storeIcon from './store/storeIcon.svg';
+
+export {
+  google,
+  kakao,
+  naver,
+  logo,
+  logoText,
+  logoWhale,
+  storeIcon,
+  mapIcon,
+  plusButton,
+  rightArrow,
+};
