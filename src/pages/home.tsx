b4750f4264fc1b378d44f7feb16c4ff25cdf8319
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-01 23:21:19 +0900

    ✨ Feat: 프레이머 모션 적용

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,26 @@
 // import Footer from '@/components/layout/Footer';
 
+import { motion } from 'framer-motion';
 import StartBtn from '@/components/home/StartBtn';
-// import { logoText, logoWhale } from '@/assets/assets';
-import { test } from '@/assets/assets';
+import { logoText, logoWhale } from '@/assets/assets';
 
 const Home = () => {
   return (
     <div className='h- flex h-[calc(100vh-75px)] items-center justify-center'>
       <div className='flex items-center gap-5'>
-        {/* <img
-          src={logoWhale}
+        <img src={logoWhale} alt='메인 로고' className='h-[200px]' />
+        <motion.img
+          src={logoText}
           alt='메인 로고'
-          className='h-[200px] animate-smoothWaveAndSlide'
+          className='h-[70px]'
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 0.5,
+            delay: 0.3,
+            ease: 'easeOut',
+          }}
         />
-        <img src={logoText} alt='메인 로고' className='h-[70px]' /> */}
-        <img src={test} />
       </div>
 
       <StartBtn />
