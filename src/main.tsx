2cb6f9288c15382e23037e7e90e1a81cfed86970
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-15 11:17:57 +0900

    ✨ Feat: React Query 프로바이더 설정 추가
    #25

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </BrowserRouter>
+  </QueryClientProvider>
 );
