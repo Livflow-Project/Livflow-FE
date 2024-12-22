7e850000daf22381d678c1c8878185cb834922c2
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-22 23:59:36 +0900

    fix: 목데이터 타입 수정

--- src/mocks/handlers/storeIdHandler.ts
+++ src/mocks/handlers/storeIdHandler.ts
@@ -13,33 +13,140 @@ type StoreIDResponse = {
 
 type DayInfo = {
   day: number;
-  day_info: DayTransaction[];
+  day_info: DayTransaction;
 };
 
 type DayTransaction = {
+  expense: DayDetailTransaction[];
+  income: DayDetailTransaction[];
+};
+
+type DayDetailTransaction = {
   category: string;
   detail: string;
   cost: number;
 };
 
 const MOCK_STORE_ID: StoreIDResponse = {
+  // 스토어 아이디
   store_id: 1,
-  name: 'Store A',
+  // 스토어 이름
+  name: '스토어 이름',
+  // 해당 달에 있는 카테고리별 총 지출
   chart: {
-    expense: [{ category: '식비', cost: 30000 }],
-    income: [{ category: '급여', cost: 2000000 }],
+    expense: [
+      {
+        category: '식비',
+        // 카테고리가 식비인 지출의 총 합
+        cost: 30000,
+      },
+    ],
+    income: [
+      {
+        category: '급여',
+        // 카테고리가 급여인 수입의 총 합
+        cost: 2000000,
+      },
+    ],
   },
+  // 해당 달에 있는 날짜별 지출, 수입
+  // 1일부터 순서대로 배열에 담기고 지출이나 수입이 있는 날짜만 받음
   date_info: [
     {
+      // 1일에 있는 상세 지출, 수입 정보
       day: 1,
-      day_info: [{ category: '식비', detail: '점심', cost: 10000 }],
+      day_info: {
+        expense: [
+          // 1일에 입력 된 상세 지출 정보
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+        ],
+        income: [
+          // 1일에 입력 된 상세 수입 정보
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+        ],
+      },
     },
     {
+      // 1일에 있는 상세 지출, 수입 정보
       day: 2,
-      day_info: [
-        { category: '교통비', detail: '택시', cost: 15000 },
-        { category: '식비', detail: '저녁', cost: 20000 },
-      ],
+      day_info: {
+        expense: [
+          // 2일에 입력 된 상세 지출 정보 전체
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+        ],
+        income: [
+          // 2일에 입력 된 상세 수입 정보 전체
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+        ],
+      },
+    },
+    {
+      // 3일에 있는 상세 지출, 수입 정보
+      day: 3,
+      day_info: {
+        expense: [
+          // 3일에 입력 된 상세 지출 정보 전체
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+          {
+            category: '식비',
+            detail: '친구랑 밥',
+            cost: 30000,
+          },
+        ],
+        income: [
+          // 3일에 입력 된 상세 수입 정보 전체
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+          {
+            category: '급여',
+            detail: '11월 급여',
+            cost: 2000000,
+          },
+        ],
+      },
     },
   ],
 };
