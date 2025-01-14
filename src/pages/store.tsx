fa22aae0419da12a60be62743db429ee0d42fe27
Yi-HyeonJu <guswwn1925@gmail.com>
2025-01-14 23:12:16 +0900

    💡 Comment: 주석 제거

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -9,7 +9,6 @@ const Store = () => {
   const [isDeleteMode, setIsDeleteMode] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // React Query 훅 사용
   const { useGetAllStores } = useStoreQuery();
   const { data, isLoading, error } = useGetAllStores();
 
@@ -30,7 +29,6 @@ const Store = () => {
     return <div>에러가 발생했습니다: {error.message}</div>;
   }
 
-  // data가 없거나 배열이 아닌 경우를 처리
   if (!data) {
     return <div>데이터를 불러올 수 없습니다.</div>;
   }
