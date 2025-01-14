a69bac9bb0177d7a26c963d05d6ec4bc6d0e004b
Yi-HyeonJu <guswwn1925@gmail.com>
2025-01-15 01:26:56 +0900

    ♻️ Refactor: 파일명 변경 및 내부 버튼 컴포넌트 분리
    #41

---
+++ src/components/store/chart/ChartView.tsx
@@ -0,0 +1,67 @@
+import { CHART_LABELS, CHART_TYPES, ChartType } from './chart.type';
+import { memo, useMemo, useState } from 'react';
+
+import { Category } from '@/api/store/store.type';
+import ChartTypeButton from './ChartTypeButton';
+import PieChart from '../../common/PieChart';
+
+type ChartViewProps = {
+  chartInfo?: Category[];
+  isDeleteMode: boolean;
+};
+
+const ChartView = ({ isDeleteMode, chartInfo }: ChartViewProps) => {
+  const [selectedType, setSelectedType] = useState<ChartType>(
+    CHART_TYPES.EXPENSE
+  );
+
+  const filteredCategories = useMemo(
+    () => chartInfo?.filter((item) => item.type === selectedType),
+    [chartInfo, selectedType]
+  );
+
+  const handleTypeChange = (type: ChartType) => {
+    setSelectedType(type);
+  };
+
+  const chartContent = useMemo(
+    () =>
+      filteredCategories && filteredCategories.length > 0 ? (
+        <PieChart selectedType={selectedType} categories={filteredCategories} />
+      ) : (
+        <div className='text-center'>
+          {`입력된 ${CHART_LABELS[selectedType]}이 없습니다.`}
+        </div>
+      ),
+    [filteredCategories, selectedType]
+  );
+
+  return (
+    <>
+      <div className='flex w-[100%] justify-between'>
+        <ChartTypeButton
+          type={CHART_TYPES.EXPENSE}
+          selectedType={selectedType}
+          isDeleteMode={isDeleteMode}
+          onClick={() => handleTypeChange(CHART_TYPES.EXPENSE)}
+        />
+
+        <ChartTypeButton
+          type={CHART_TYPES.INCOME}
+          selectedType={selectedType}
+          isDeleteMode={isDeleteMode}
+          onClick={() => handleTypeChange(CHART_TYPES.INCOME)}
+        />
+      </div>
+
+      <div className={isDeleteMode ? 'opacity-50' : ''}>{chartContent}</div>
+    </>
+  );
+};
+
+export default memo(ChartView, (prevProps, nextProps) => {
+  return (
+    prevProps.isDeleteMode === nextProps.isDeleteMode &&
+    JSON.stringify(prevProps.chartInfo) === JSON.stringify(nextProps.chartInfo)
+  );
+});
