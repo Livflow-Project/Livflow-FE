ac80a61e2cff312abd347701360590d6c5bf9002
Yi-HyeonJu <guswwn1925@gmail.com>
2025-01-21 20:06:05 +0900

    ✨ Feat: 수정/삭제 모드 시에 완료버튼만 화면에 표시
    #37

--- src/components/storeId/dailyDetails/buttons/ActionButtons.tsx
+++ src/components/storeId/dailyDetails/buttons/ActionButtons.tsx
@@ -8,19 +8,27 @@ const ActionButtons = ({
   isEditMode,
   onEditModeToggle,
   onModalOpen,
-}: ActionButtonsProps) => (
-  <div className='flex w-full items-center justify-between px-[25px] pb-[20px]'>
-    <button onClick={onModalOpen} className='soft_BcolorSet w-[40%]'>
-      지출 / 수입 추가하기
-    </button>
+}: ActionButtonsProps) => {
+  if (isEditMode) {
+    return (
+      <div className='flex w-full items-center justify-end px-[25px] pb-[20px]'>
+        <button onClick={onEditModeToggle} className='soft_BcolorSet w-[25%]'>
+          완료하기
+        </button>
+      </div>
+    );
+  }
 
-    <button
-      onClick={onEditModeToggle}
-      className={`soft_BcolorSet w-[25%] ${isEditMode ? 'bg-main text-white' : ''}`}
-    >
-      수정 / 삭제 하기
-    </button>
-  </div>
-);
+  return (
+    <div className='flex w-full items-center justify-between px-[25px] pb-[20px]'>
+      <button onClick={onModalOpen} className='soft_BcolorSet w-[40%]'>
+        지출 / 수입 추가하기
+      </button>
+      <button onClick={onEditModeToggle} className='soft_BcolorSet w-[25%]'>
+        수정 / 삭제 하기
+      </button>
+    </div>
+  );
+};
 
 export default ActionButtons;
