b89c8faeed89c2994bb6355d2131dfa50faac6a0
Yi-HyeonJu <guswwn1925@gmail.com>
2025-01-27 20:27:08 +0900

    ✨ Feat: 라우트 파일 인덱스 파일에 연결
    #45

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,13 @@ import { HeaderlessRoutes } from './HeaderlessRoutes';
 import { RouterProvider } from 'react-router';
 import { commonRoutes } from './commonRoutes';
 import { createBrowserRouter } from 'react-router-dom';
+import { loggedRoutes } from './loggedRoutes';
 
 const Router = () => {
   const router = createBrowserRouter([
     ...commonRoutes, // 기본 헤더 라우트
     ...HeaderlessRoutes, // 헤더 없는 라우드
+    ...loggedRoutes, // 로그인 된 라우트
   ]);
 
   return (
