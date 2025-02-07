e12ed2608e2cf2ad1b4b6b2cc548d000825ebbc1
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-07 23:28:33 +0900

    ✨ Feat: 에러바운더리 설정
    #54

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import 'react-toastify/dist/ReactToastify.css';
 
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorPage from './pages/status/errorPage';
 import Router from './router';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
-    <>
+    <ErrorBoundary
+      fallbackRender={({ error, resetErrorBoundary }) => (
+        <ErrorPage error={error} resetError={resetErrorBoundary} />
+      )}
+      onError={(error) => {
+        error;
+      }}
+    >
       <ToastContainer />
       <Router />
-    </>
+    </ErrorBoundary>
   );
 };
 
