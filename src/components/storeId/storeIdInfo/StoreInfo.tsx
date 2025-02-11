220c142a453f65821ed409d047633d70105fde38
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-12 00:44:40 +0900

    🔥 Remove: 메모제이션 코드 삭제
    #55

--- src/components/storeId/storeIdInfo/StoreInfo.tsx
+++ src/components/storeId/storeIdInfo/StoreInfo.tsx
@@ -1,22 +1,19 @@
-import { memo } from 'react';
 import { useStore } from '@/contexts/StoreContext';
 
-const StoreInfo = memo(() => {
+const StoreInfo = () => {
   const { storeInfo } = useStore();
 
   if (!storeInfo) return null;
 
   return (
     <ul className='flex flex-col items-end gap-4'>
-      <li className='text-2xl font-semibold text-main'>
-        {storeInfo.name || '이름 정보 없음'}
-      </li>
+      <li className='text-2xl font-semibold text-main'>{storeInfo.name}</li>
       <li className='text-[15px] font-medium text-main'>
         {storeInfo.address || '주소 정보 없음'}
       </li>
     </ul>
   );
-});
+};
 
 StoreInfo.displayName = 'StoreInfo';
 
