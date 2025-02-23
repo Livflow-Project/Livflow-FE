2f3c9f288ebc28492685f5bfde90abe6da960115
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-24 00:00:16 +0900

    🎨 Design: 메뉴 부분 디자인 작업 중
    #61

--- src/components/storeId/costCalculator/MainRecipe.tsx
+++ src/components/storeId/costCalculator/MainRecipe.tsx
@@ -1,7 +1,14 @@
 const MainRecipe = () => {
   return (
-    <div>
-      <span>MainRecipe</span>
+    <div className='flex h-full items-center justify-center'>
+      <ul className='grid grid-cols-4 gap-x-20 gap-y-10'>
+        {Array.from({ length: 8 }).map((_, index) => (
+          <li
+            key={index}
+            className='border-gray h-[250px] w-[200px] rounded-lg border border-opacity-20 bg-white shadow-xl'
+          ></li>
+        ))}
+      </ul>
     </div>
   );
 };
