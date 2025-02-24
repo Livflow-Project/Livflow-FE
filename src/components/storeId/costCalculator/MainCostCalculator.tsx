fa074940d1a2b27088ef7b9dc107020ed2a4ca09
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-25 00:07:59 +0900

    ✨ Feat: 레시피 추가, 삭제 기능 임시 구현
    #63

--- src/components/storeId/costCalculator/MainCostCalculator.tsx
+++ src/components/storeId/costCalculator/MainCostCalculator.tsx
@@ -1,39 +1,36 @@
-// type MainCostCalculatorProps = {
-//   storeId: string;
-// };
-
 import Button from '@/components/common/Button';
-import CostCalculatorHeader from './components/costCalculatorTable/CostCalculatorHeader';
-import Header from './components/Header';
-import ImageUploader from './components/ImageUploader';
-import SummaryInfo from './components/SummaryInfo';
+import CostCalculatorHeader from './components/costCalculator/costCalculatorTable/CostCalculatorHeader';
+import Header from './components/costCalculator/Header';
+import ImageUploader from './components/costCalculator/ImageUploader';
+import SummaryInfo from './components/costCalculator/SummaryInfo';
 
-// const MainCostCalculator = ({ storeId }: MainCostCalculatorProps) => {
+type MainCostCalculatorProps = {
+  onSave: () => void;
+  onCancel: () => void;
+};
 
-const MainCostCalculator = () => {
+const MainCostCalculator = ({ onSave, onCancel }: MainCostCalculatorProps) => {
   return (
-    <>
-      <div className='flex h-full justify-between px-[35px] py-[30px]'>
-        <div className='flex h-full w-[70%] flex-col justify-between'>
-          <Header />
+    <div className='flex h-full justify-between px-[35px] py-[30px]'>
+      <div className='flex h-full w-[70%] flex-col justify-between'>
+        <Header />
 
-          <article className='h-[calc(100%-80px)] w-full rounded-xl bg-white'>
-            <CostCalculatorHeader />
-          </article>
-        </div>
+        <article className='h-[calc(100%-80px)] w-full rounded-xl bg-white'>
+          <CostCalculatorHeader />
+        </article>
+      </div>
 
-        <div className='flex h-full w-[28%] flex-col justify-between'>
-          <SummaryInfo />
+      <div className='flex h-full w-[28%] flex-col justify-between'>
+        <SummaryInfo />
 
-          <ImageUploader />
+        <ImageUploader />
 
-          <div className='flex items-center justify-between'>
-            <Button children={' 취소하기'} />
-            <Button children={'메뉴 저장하기'} />
-          </div>
+        <div className='flex items-center justify-between'>
+          <Button onClick={onCancel}>취소하기</Button>
+          <Button onClick={onSave}>메뉴 저장하기</Button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
