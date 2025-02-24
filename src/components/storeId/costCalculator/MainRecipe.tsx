1cd58126b471fb3668ebd1814c3f83d40c1f8919
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-25 00:11:41 +0900

    ✨ Feat: 모든 메뉴 삭제 시 삭제 모드 해제
    #63

--- src/components/storeId/costCalculator/MainRecipe.tsx
+++ src/components/storeId/costCalculator/MainRecipe.tsx
@@ -1,14 +1,78 @@
+import { useEffect, useState } from 'react';
+
+import Button from '@/components/common/Button';
+import MainCostCalculator from './MainCostCalculator';
+import RecipeList from './components/recipe/RecipeList';
+
+type Recipe = {
+  id: number;
+};
+
 const MainRecipe = () => {
+  const [showCostCalculator, setShowCostCalculator] = useState(false);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [isDeleteMode, setIsDeleteMode] = useState(false);
+
+  const handleAddMenu = () => {
+    setShowCostCalculator(true);
+  };
+
+  const handleSaveMenu = () => {
+    setRecipes((prevRecipes) => [...prevRecipes, { id: Date.now() }]);
+    setShowCostCalculator(false);
+  };
+
+  const handleCancelMenu = () => {
+    setShowCostCalculator(false);
+  };
+
+  const handleDeleteMode = () => {
+    setIsDeleteMode((prevMode) => !prevMode);
+  };
+
+  const handleDeleteRecipe = (id: number) => {
+    setRecipes((prevRecipes) => {
+      const updatedRecipes = prevRecipes.filter((recipe) => recipe.id !== id);
+      if (updatedRecipes.length === 0) {
+        setIsDeleteMode(false); // 모든 레시피가 삭제되면 삭제 모드 종료
+      }
+      return updatedRecipes;
+    });
+  };
+
+  useEffect(() => {
+    if (recipes.length === 0) {
+      setIsDeleteMode(false);
+    }
+  }, [recipes]);
+
   return (
-    <div className='flex h-full items-center justify-center'>
-      <ul className='grid grid-cols-4 gap-x-20 gap-y-10'>
-        {Array.from({ length: 8 }).map((_, index) => (
-          <li
-            key={index}
-            className='border-gray h-[250px] w-[200px] rounded-lg border border-opacity-20 bg-white shadow-xl'
-          ></li>
-        ))}
-      </ul>
+    <div className='flex h-full flex-col'>
+      {showCostCalculator ? (
+        <MainCostCalculator
+          onSave={handleSaveMenu}
+          onCancel={handleCancelMenu}
+        />
+      ) : (
+        <>
+          <div className='flex items-center justify-center gap-20 py-[30px]'>
+            {isDeleteMode ? (
+              <Button onClick={handleDeleteMode}>완료하기</Button>
+            ) : (
+              <>
+                <Button onClick={handleAddMenu}>메뉴 추가하기</Button>
+                <Button onClick={handleDeleteMode}>메뉴 삭제하기</Button>
+              </>
+            )}
+          </div>
+
+          <RecipeList
+            recipes={recipes}
+            isDeleteMode={isDeleteMode}
+            onDeleteRecipe={handleDeleteRecipe}
+          />
+        </>
+      )}
     </div>
   );
 };
