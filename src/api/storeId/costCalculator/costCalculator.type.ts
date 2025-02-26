a842fa121708b08bbf8dd2465178c8c5e3fd18ef
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-26 23:08:31 +0900

    ✨ Feat: 원가계산 타입 파일 추가
    #63

---
+++ src/api/storeId/costCalculator/costCalculator.type.ts
@@ -0,0 +1,26 @@
+export type CostCalculatorListItem = {
+  recipe_id: string; // UUID
+  recipe_name: string;
+  recipe_cost?: number;
+  recipe_img?: string;
+};
+
+export type RecipeIngredient = {
+  ingredient_id: string; // UUID
+  ingredient_name: string;
+  required_amount: number;
+  unit: 'ml' | 'g' | 'ea';
+};
+
+export type CostCalculatorDetail = {
+  recipe_id: string; // UUID
+  recipe_name: string;
+  recipe_cost?: number;
+  recipe_img?: string;
+  ingredients?: RecipeIngredient[];
+  total_ingredient_cost?: number;
+  production_quantity?: number;
+  production_cost?: number;
+};
+
+export type CostCalculatorRequest = Omit<CostCalculatorDetail, 'recipe_id'>;
