3476d076ffbf79e2d1cb55c4c7e3f39e8ec93bac
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-26 22:16:12 +0900

    ✨ Feat: 훅 파일 추가
    #64

---
+++ src/api/storeId/inventory/inventory.hooks.ts
@@ -0,0 +1,31 @@
+import { InventoryItem, UseInventoryItemRequest } from './inventory.type';
+import { getInventoryItemsAPI, useInventoryItemAPI } from './inventoryAPI';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+const INVENTORY_QUERY_KEY = 'inventory';
+
+export const useGetInventoryItems = (storeId: string) => {
+  return useQuery<InventoryItem[]>({
+    queryKey: [INVENTORY_QUERY_KEY, storeId],
+    queryFn: () => getInventoryItemsAPI(storeId),
+  });
+};
+
+export const useUseInventoryItemMutation = (storeId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      ingredientId,
+      data,
+    }: {
+      ingredientId: string;
+      data: UseInventoryItemRequest;
+    }) => useInventoryItemAPI(storeId, ingredientId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [INVENTORY_QUERY_KEY, storeId],
+      });
+    },
+  });
+};
