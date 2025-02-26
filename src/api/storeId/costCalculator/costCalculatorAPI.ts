d56e06064f8a99c1497b56dbcd3c66535160f838
Yi-HyeonJu <guswwn1925@gmail.com>
2025-02-26 23:08:42 +0900

    ✨ Feat: 원가계산 API 파일 추가
    #63

---
+++ src/api/storeId/costCalculator/costCalculatorAPI.ts
@@ -0,0 +1,53 @@
+import {
+  CostCalculatorDetail,
+  CostCalculatorListItem,
+  CostCalculatorRequest,
+} from './costCalculator.type';
+
+import axiosInstance from '@/api/axiosInstance';
+
+export const getAllRecipesAPI = async (
+  storeId: string
+): Promise<CostCalculatorListItem[]> => {
+  console.log('API 응답:', storeId);
+  const response = await axiosInstance.get(`/costcalcul/${storeId}`);
+  console.log('API 응답:', response.data);
+  return response.data;
+};
+
+export const getRecipeAPI = async (
+  storeId: string,
+  recipeId: string
+): Promise<CostCalculatorDetail> => {
+  const response = await axiosInstance.get(
+    `/costcalcul/${storeId}/${recipeId}`
+  );
+  return response.data;
+};
+
+export const createRecipeAPI = async (
+  storeId: string,
+  data: CostCalculatorRequest
+): Promise<CostCalculatorDetail> => {
+  const response = await axiosInstance.post(`/costcalcul/${storeId}`, data);
+  return response.data;
+};
+
+export const updateRecipeAPI = async (
+  storeId: string,
+  recipeId: string,
+  data: CostCalculatorRequest
+): Promise<CostCalculatorDetail> => {
+  const response = await axiosInstance.put(
+    `/costcalcul/${storeId}/${recipeId}`,
+    data
+  );
+  return response.data;
+};
+
+export const deleteRecipeAPI = async (storeId: string, recipeId: string) => {
+  const response = await axiosInstance.delete(
+    `/costcalcul/${storeId}/${recipeId}`
+  );
+  return response.data;
+};
