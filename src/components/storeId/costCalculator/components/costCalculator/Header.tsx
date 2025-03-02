dc046610c0889ac4e8d90f258da8c50bf67ba086
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-02 17:25:37 +0900

    🎨 Design: 인풋 디자인 작업
    #61

--- src/components/storeId/costCalculator/components/costCalculator/Header.tsx
+++ src/components/storeId/costCalculator/components/costCalculator/Header.tsx
@@ -2,34 +2,40 @@ const Header = () => {
   return (
     <header className='flex h-[60px] w-full items-center justify-evenly rounded-lg bg-white text-xl'>
       <div className='flex w-[40%] items-center justify-center'>
-        <span className='mr-2'>메뉴 이름 : </span>
-        <input
-          type='text'
-          className='h-full border-0 border-b border-gray-300 bg-transparent p-0 text-lg focus:border-b focus:border-gray-300 focus:outline-none focus:ring-0'
-        />
+        <label htmlFor='menu_name' className='input_label mr-2'>
+          메뉴 이름 :
+        </label>
+        <input id='menu_name' type='text' className='input_underlined h-full' />
       </div>
 
       <div className='h-[40px] w-[1px] bg-caption'></div>
 
       <div className='flex w-[40%] items-center justify-center'>
-        <span className='mr-2'>판매 가격 : </span>
+        <label htmlFor='menu_cost' className='input_label mr-2'>
+          판매 가격 :
+        </label>
         <input
+          id='menu_cost'
           type='number'
-          className='h-full border-0 border-b border-gray-300 bg-transparent p-0 text-lg [-moz-appearance:_textfield] focus:border-b focus:border-gray-300 focus:outline-none focus:ring-0 [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none'
+          className='input_underlined number_input h-full'
         />
-        <span className='ml-1'>원</span>
+        <span className='input_label ml-1'>원</span>
       </div>
 
       <div className='h-[40px] w-[1px] bg-caption'></div>
 
       <div className='flex w-[15%] items-center justify-center'>
-        <label className='flex cursor-pointer items-center'>
-          <span className='mr-2'>즐겨찾기</span>
-          <input
-            type='checkbox'
-            className='h-6 w-6 rounded border-gray-300 text-green shadow-sm focus:ring-0 focus:ring-offset-0'
-          />
+        <label
+          htmlFor='favorites_checkbox'
+          className='input_label mr-2 cursor-pointer'
+        >
+          즐겨찾기
         </label>
+        <input
+          id='favorites_checkbox'
+          type='checkbox'
+          className='h-6 w-6 cursor-pointer rounded border-gray-300 text-green shadow-sm focus:ring-0 focus:ring-offset-0'
+        />
       </div>
     </header>
   );
--- src/components/storeId/costCalculator/components/costCalculator/SummaryInfo.tsx
+++ src/components/storeId/costCalculator/components/costCalculator/SummaryInfo.tsx
@@ -1,10 +1,24 @@
 const SummaryInfo = () => {
   return (
     <article className='h-[30%] w-full rounded-xl bg-white/50'>
-      <ul className='flex h-full flex-col items-start justify-evenly px-5 text-xl font-semibold text-main'>
-        <li>총 재료 원가 : </li>
-        <li>생산 수량 : </li>
-        <li>생산 단가 </li>
+      <ul className='flex h-full flex-col items-start justify-evenly px-5'>
+        <li className='input_label'>총 재료 원가 :</li>
+
+        <li className='flex w-full items-center'>
+          <label
+            htmlFor='production_quantity'
+            className='input_label mr-2 whitespace-nowrap'
+          >
+            생산 수량 :
+          </label>
+          <input
+            id='production_quantity'
+            type='number'
+            className='input_underlined number_input flex-1'
+          />
+        </li>
+
+        <li className='input_label'>생산 단가 :</li>
       </ul>
     </article>
   );
