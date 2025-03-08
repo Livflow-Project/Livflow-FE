bba27af1fa7cd5810a656bf421c8bc47fe52d906
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-09 02:20:27 +0900

    🎨 Design: 높이 지정

--- src/components/storeId/storeIdInfo/StoreInfo.tsx
+++ src/components/storeId/storeIdInfo/StoreInfo.tsx
@@ -6,7 +6,7 @@ const StoreInfo = () => {
   if (!storeInfo) return null;
 
   return (
-    <ul className='flex flex-col items-end gap-4'>
+    <ul className='flex h-[70px] flex-col items-end justify-between'>
       <li className='text-2xl font-semibold text-main'>{storeInfo.name}</li>
       <li className='text-[15px] font-medium text-main'>
         {storeInfo.address || '주소 정보 없음'}
