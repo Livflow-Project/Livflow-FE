a52af4501f984aeea98895b3d5da271727874f31
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-08 22:22:17 +0900

    🎨 Design:  거래 존재 여부에 따라 다르게 렌더링
    #74

--- src/components/storeId/ledger/transaction/components/button/ActionButtons.tsx
+++ src/components/storeId/ledger/transaction/components/button/ActionButtons.tsx
@@ -1,28 +1,43 @@
 import Button from '@/components/common/Button';
+import { twMerge } from 'tailwind-merge';
 
 type ActionButtonsProps = {
   isEditMode: boolean;
   onEditModeToggle: () => void;
   onModalOpen: () => void;
+  hasTransactions?: boolean;
 };
 
 const ActionButtons = ({
   isEditMode,
   onEditModeToggle,
   onModalOpen,
+  hasTransactions = false,
 }: ActionButtonsProps) => {
   if (isEditMode) {
     return (
-      <div className='flex w-full items-center justify-end px-[25px] pb-[20px]'>
+      <div
+        className={twMerge(
+          'flex w-full items-center justify-end px-[25px]',
+          hasTransactions ? 'py-[20px]' : 'pb-[20px]'
+        )}
+      >
         <Button onClick={onEditModeToggle}>완료하기</Button>
       </div>
     );
   }
 
   return (
-    <div className='flex w-full items-center justify-between px-[25px] py-[20px]'>
+    <div
+      className={twMerge(
+        'flex w-full items-center px-[25px]',
+        hasTransactions ? 'justify-between py-[20px]' : 'justify-end pb-[20px]'
+      )}
+    >
       <Button onClick={onModalOpen}>지출 / 수입 추가하기</Button>
-      <Button onClick={onEditModeToggle}>수정 / 삭제 하기</Button>
+      {hasTransactions && (
+        <Button onClick={onEditModeToggle}>수정 / 삭제 하기</Button>
+      )}
     </div>
   );
 };
