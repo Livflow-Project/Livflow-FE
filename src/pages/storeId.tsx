9895b152f27cd07c32cae1931d9ca005b4e14581
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-08 22:37:15 +0900

    ♻️ Refactor: useNavigate 대신 NavLink 사용
    #69

--- src/pages/storeId.tsx
+++ src/pages/storeId.tsx
@@ -1,5 +1,5 @@
-import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
-import { Suspense, useEffect, useRef } from 'react';
+import { NavLink, Outlet, useParams } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
 
 import ContentLoadingIndicator from '@/components/common/ContentLoadingIndicator';
 import ErrorPage from './status/errorPage';
@@ -17,31 +17,28 @@ const NAV_ITEMS = [
 
 const StoreId = () => {
   const { id } = useParams<{ id: string }>();
-  const location = useLocation();
-  const navigate = useNavigate();
 
   // 스토어 관련 훅
-  const { setStoreInfo } = useStore();
+  const { storeInfo, setStoreInfo } = useStore();
   const { useGetStore } = useStoreQuery();
-  const { data, isError, error, refetch } = useGetStore(id || '0');
-  const isInitialized = useRef(false);
 
-  // 현재 활성화된 탭 확인
-  const currentPath = location.pathname.split('/').pop() || 'ledger';
+  const { data, isError, error, refetch } = useGetStore(id || '0');
 
   // 스토어 정보 설정
   useEffect(() => {
-    if (data && !isInitialized.current) {
-      setStoreInfo({
-        id: id,
-        name: data.name,
-        address: data.address,
-      });
-      isInitialized.current = true;
+    if (data) {
+      // 현재 ID와 새 데이터의 ID가 다를 때만 업데이트
+      if (!storeInfo || storeInfo.id !== id) {
+        setStoreInfo({
+          id: id,
+          name: data.name,
+          address: data.address,
+        });
+      }
     }
-  }, [data, setStoreInfo, id]);
+  }, [data, storeInfo, setStoreInfo, id]);
 
-  if (isError || !id) {
+  if (isError) {
     return <ErrorPage error={error as Error} resetError={() => refetch()} />;
   }
 
@@ -52,33 +49,35 @@ const StoreId = () => {
       <div className='h-[calc(100%-70px)]'>
         <div className='flex items-center justify-start gap-3'>
           {NAV_ITEMS.map((item) => (
-            <nav
+            <NavLink
               key={item.path}
-              onClick={() => navigate(`/store/${id}/${item.path}`)}
-              className={twMerge(
-                'flex h-[55px] w-[135px] cursor-pointer items-center justify-center rounded-tl-[10px] rounded-tr-[10px]',
-                currentPath === item.path
-                  ? 'bg-background'
-                  : 'bg-background/50 hover:bg-background'
-              )}
+              to={`/store/${id}/${item.path}`}
+              className={({ isActive }) =>
+                twMerge(
+                  'group flex h-[55px] w-[135px] cursor-pointer items-center justify-center rounded-tl-[10px] rounded-tr-[10px]',
+                  isActive
+                    ? 'bg-background'
+                    : 'bg-background/50 hover:bg-background'
+                )
+              }
             >
-              <span
-                className={twMerge(
-                  'text-[22px] font-normal',
-                  currentPath === item.path
-                    ? 'font-semibold text-main'
-                    : 'text-main/50'
-                )}
-              >
-                {item.title}
-              </span>
-            </nav>
+              {({ isActive }) => (
+                <span
+                  className={twMerge(
+                    'text-[22px] font-normal group-hover:font-semibold',
+                    isActive ? 'font-semibold text-main' : 'text-main/50'
+                  )}
+                >
+                  {item.title}
+                </span>
+              )}
+            </NavLink>
           ))}
         </div>
 
-        <div className='h-[calc(100%-55px)] w-full bg-background'>
+        <div className='h-[calc(100%-55px)] w-full rounded-bl-lg rounded-br-lg rounded-tr-lg bg-background'>
           <Suspense fallback={<ContentLoadingIndicator />}>
-            <Outlet context={{ storeId: id }} />
+            <Outlet />
           </Suspense>
         </div>
       </div>
