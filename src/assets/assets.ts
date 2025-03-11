3e4b47bdc92f47291d227b4586be2b4c2d50cca4
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-12 01:52:23 +0900

    🎨 Design: svg들 임포트
    #75

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,10 @@
+import calculator from './home/calculator.svg';
 import favoritesIcon from './store/favoritesIcon.svg';
 import google from './login/google.svg';
 import imageIcon from './store/imageIcon.svg';
+import ingredient from './home/ingredient.svg';
 import kakao from './login/kakao.svg';
+import ledger from './home/ledger.svg';
 import logo from './header/logo.svg';
 import logoText from './home/logoText.svg';
 import logoWhale from './home/logoWhale.svg';
@@ -9,11 +12,14 @@ import mapIcon from './store/mapIcon.svg';
 import naver from './login/naver.svg';
 import plusButton from './store/plusButton.svg';
 import rightArrow from './home/rightArrow.svg';
+import store from './home/store.svg';
 import storeIcon from './store/storeIcon.svg';
-import test from './home/test.svg';
 
 export {
-  test,
+  store,
+  ledger,
+  ingredient,
+  calculator,
   google,
   kakao,
   naver,
