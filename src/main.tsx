6d664c45ef2bc1ece4f7d047c8bb970700285549
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-13 01:12:42 +0900

    ♻️ Refactor: 목데이터 사용 정지
    #69

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,20 @@ import { createRoot } from 'react-dom/client';
 
 const queryClient = new QueryClient();
 
-async function enableMocking() {
-  if (process.env.NODE_ENV === 'development') {
-    const { worker } = await import('./mocks/browser');
-    return worker.start({
-      onUnhandledRequest: 'bypass', // 처리되지 않은 요청 무시
-    });
-  }
-}
+// async function enableMocking() {
+//   if (process.env.NODE_ENV === 'development') {
+//     const { worker } = await import('./mocks/browser');
+//     return worker.start({
+//       onUnhandledRequest: 'bypass', // 처리되지 않은 요청 무시
+//     });
+//   }
+// }
 
 // MSW 초기화 후 앱 렌더링
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  );
-});
+// enableMocking().then(() => {
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
+// });
