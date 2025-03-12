2f48afe42ceb24425bcbffecfbe57cab2bab82fc
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-13 01:08:36 +0900

    🎨 Design: 부모 ui 태그 상위 컴포넌트로 이동
    #75

--- src/components/storeId/storeIdInfo/StoreInfo.tsx
+++ src/components/storeId/storeIdInfo/StoreInfo.tsx
@@ -1,17 +1,18 @@
+import ErrorPage from '@/pages/status/errorPage';
 import { useStore } from '@/contexts/StoreContext';
 
 const StoreInfo = () => {
   const { storeInfo } = useStore();
 
-  if (!storeInfo) return null;
+  if (!storeInfo) return <ErrorPage />;
 
   return (
-    <ul className='flex h-[70px] flex-col items-end justify-between'>
+    <>
       <li className='text-2xl font-semibold text-main'>{storeInfo.name}</li>
       <li className='text-[15px] font-medium text-main'>
         {storeInfo.address || '주소 정보 없음'}
       </li>
-    </ul>
+    </>
   );
 };
 
