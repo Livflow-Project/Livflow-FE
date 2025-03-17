f19bbffad25ba0d0d78ef8abaa85fc302429a3dc
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-17 23:48:55 +0900

    ✨ Feat: 스와이퍼 기본 설정 파일 추가
    #84

---
+++ src/components/store/swiper/swiperConfig.ts
@@ -0,0 +1,11 @@
+import { Navigation, Pagination } from 'swiper/modules';
+
+export const storeListSwiperConfig = {
+  modules: [Navigation, Pagination],
+  navigation: true,
+  pagination: { clickable: true },
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 30,
+  className: 'h-[650px]',
+};
