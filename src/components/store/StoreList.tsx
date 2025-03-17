c9ddc8ba9fef99d4a1691d3437d4f6c10d6879a0
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-17 23:49:26 +0900

    ♻️ Refactor: 분리한 스와이퍼 파일 적용
    #84

--- src/components/store/StoreList.tsx
+++ src/components/store/StoreList.tsx
@@ -1,11 +1,12 @@
 import 'swiper/swiper-bundle.css';
 
-import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import AddStore from './components/AddStore';
 import MyStore from './components/MyStore';
 import { StoreDetailResponse } from '@/api/store/store.type';
+import { storeListSwiperConfig } from './swiper/swiperConfig';
+import { storeListSwiperStyles } from './swiper/swiperStyles';
 
 type StoreListProps = {
   stores: StoreDetailResponse[];
@@ -18,16 +19,6 @@ const StoreList = ({
   onToggleModal,
   isDeleteMode,
 }: StoreListProps) => {
-  const swiperConfig = {
-    modules: [Navigation, Pagination],
-    navigation: true,
-    pagination: { clickable: true },
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 30,
-    className: 'h-[650px]',
-  };
-
   const renderStores = () => {
     const slides = [];
     // 페이지당 2개의 스토어로 변경
@@ -56,42 +47,10 @@ const StoreList = ({
     <div className='relative flex max-w-[1200px]'>
       <AddStore onOpenModal={onToggleModal} />
 
-      <style>
-        {`
-            .swiper-container {
-              position: relative;
-              z-index: 1;
-            }
-            .swiper {
-              width: 100% !important;
-              max-width: 860px !important;
-              overflow: hidden !important;
-              position: relative;
-            }
-            .swiper-slide {
-              height: auto !important;
-              width: 100% !important;
-              max-width: 860px !important;
-              padding: 0 0 0 75px;
-            }
-            .swiper-button-prev,
-            .swiper-button-next {
-              color: #000;
-              opacity: 1;
-              z-index: 20;
-            }
-            .swiper-button-prev {
-              left: 30px !important;
-            }
-            .swiper-button-next {
-              right: 30px;
-            }
-            .swiper-wrapper {
-              align-items: flex-start;
-            }
-          `}
-      </style>
-      <Swiper {...swiperConfig}>{stores.length > 0 && renderStores()}</Swiper>
+      <style>{storeListSwiperStyles}</style>
+      <Swiper {...storeListSwiperConfig}>
+        {stores.length > 0 && renderStores()}
+      </Swiper>
     </div>
   );
 };
