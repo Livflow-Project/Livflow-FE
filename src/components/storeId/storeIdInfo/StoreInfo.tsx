7aaab6664c802461c2d9413cb6fb6c2a6f7acd11
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-20 01:58:37 +0900

    🎨 Design: 스토어 주소 색상 변경
    #77

--- src/components/storeId/storeIdInfo/StoreInfo.tsx
+++ src/components/storeId/storeIdInfo/StoreInfo.tsx
@@ -9,7 +9,7 @@ const StoreInfo = () => {
   return (
     <>
       <li className='text-2xl font-semibold text-main'>{storeInfo.name}</li>
-      <li className='text-[15px] font-medium text-main'>
+      <li className='text-[15px] font-medium text-caption'>
         {storeInfo.address || '주소 정보 없음'}
       </li>
     </>
