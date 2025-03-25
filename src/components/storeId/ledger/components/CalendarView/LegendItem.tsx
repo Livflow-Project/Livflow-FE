f5b8419780802e968f85fd746b5953aad2487914
Yi-HyeonJu <guswwn1925@gmail.com>
2025-03-25 15:42:46 +0900

    ✨ Feat: 범례 컴포넌트 파일 분리
    #86

---
+++ src/components/storeId/ledger/components/CalendarView/LegendItem.tsx
@@ -0,0 +1,13 @@
+type LegendItemProps = {
+  color: string;
+  label: string;
+};
+
+const LegendItem = ({ color, label }: LegendItemProps) => (
+  <div className='flex items-center gap-2'>
+    <span className={`text-${color}`}>●</span>
+    <span className='text-lg font-medium text-main'>{label}</span>
+  </div>
+);
+
+export default LegendItem;
