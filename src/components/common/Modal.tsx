c4ebf473615cd6bd365f0e6ddb7d76ccb48b1e90
Yi-HyeonJu <guswwn1925@gmail.com>
2025-04-03 23:29:03 +0900

    ♻️ Refactor: 모달 닫는 훅 적용
    #95

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,6 @@
 import { toast } from 'react-toastify';
+import { useClickOutside } from '@/hooks/useClickOutside';
+import { useRef } from 'react';
 
 type ModalProps = {
   onClose: () => void;
@@ -7,19 +9,17 @@ type ModalProps = {
 };
 
 const Modal = ({ onClose, onSubmit, children }: ModalProps) => {
-  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-      toast.dismiss();
-    }
-  };
+  const modalRef = useRef<HTMLFormElement | null>(null);
+
+  useClickOutside(modalRef, () => {
+    onClose();
+    toast.dismiss();
+  });
 
   return (
-    <div
-      className='fixed inset-0 z-50 flex items-center justify-center bg-m_background/70'
-      onClick={handleBackdropClick}
-    >
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-m_background/70'>
       <form
+        ref={modalRef}
         onSubmit={onSubmit}
         autoComplete='off'
         className='modal_div flex w-[520px] flex-col gap-7'
