fdae2facd9ae9bf61984c52d0cd64bfba1160a54
Yi-HyeonJu <guswwn1925@gmail.com>
2025-04-06 07:26:54 +0900

    ✨ Feat: 오류가 발생했을 때 추가적인 처리 코드 추가
    #92 #95

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,31 @@ import ErrorPage from './pages/status/errorPage';
 import Router from './router';
 import { StoreProvider } from './contexts/StoreContext';
 import { ToastContainer } from 'react-toastify';
+import { showErrorToast } from './utils/toast';
 
 const App = () => {
   return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => (
-        <ErrorPage error={error} resetError={resetErrorBoundary} />
-      )}
-      onError={(error) => {
-        error;
-      }}
-    >
-      <AuthProvider>
-        <StoreProvider>
-          <ToastContainer />
-          <Router />
-        </StoreProvider>
-      </AuthProvider>
-    </ErrorBoundary>
+    <>
+      <ToastContainer />
+
+      <ErrorBoundary
+        fallbackRender={({ error, resetErrorBoundary }) => (
+          <ErrorPage error={error} resetError={resetErrorBoundary} />
+        )}
+        onError={(error) => {
+          <ErrorPage error={error} resetError={() => {}} />;
+
+          console.error('에러바운더리가 잡은 에러:', error);
+          showErrorToast('오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+        }}
+      >
+        <AuthProvider>
+          <StoreProvider>
+            <Router />
+          </StoreProvider>
+        </AuthProvider>
+      </ErrorBoundary>
+    </>
   );
 };
 
