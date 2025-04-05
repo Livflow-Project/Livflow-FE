0d7e5e327a10404232930c4d7bd02741af3e16df
Yi-HyeonJu <guswwn1925@gmail.com>
2025-04-06 04:58:48 +0900

    🎨 Design: 반응형 디자인 추가
    #93

--- src/components/storeId/ledger/components/monthlyOverview/CategorySection.tsx
+++ src/components/storeId/ledger/components/monthlyOverview/CategorySection.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { ChartOverview } from '@/api/storeId/ledger/calendar/calendar.type';
 import PieChart from '@/components/common/PieChart';
+import { twMerge } from 'tailwind-merge';
 
 type CategorySectionProps = {
   title: string;
@@ -14,45 +17,100 @@ const CategorySection = ({
   total,
   isIncome,
 }: CategorySectionProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [visibleCount, setVisibleCount] = useState(5);
+
   const filteredCategories = categories.filter(
     (cat) => cat.type === (isIncome ? 'income' : 'expense')
   );
+  const getCategoryClass = (name: string) =>
+    `bg-category-${name.replace(/\s/g, '-').replace(/\./g, '')}`;
+
+  // Resize observer로 높이 측정
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const height = entry.contentRect.height;
+
+        if (height <= 308) {
+          setVisibleCount(0);
+        } else if (height <= 356) {
+          setVisibleCount(3);
+        } else if (height <= 404) {
+          setVisibleCount(4);
+        } else {
+          setVisibleCount(5);
+        }
+      }
+    });
+
+    observer.observe(container);
+    return () => observer.disconnect();
+  }, []);
 
   if (filteredCategories.length === 0) {
     return (
-      <p className='text-sx text-caption'>
+      <div className='text-sx flex h-full w-[40%] items-center justify-center text-caption'>
         추가된 {isIncome ? '수입' : '지출'}이 없습니다.
-      </p>
+      </div>
     );
   }
 
   return (
-    <div className='flex h-full w-[40%] flex-col items-center justify-center gap-[40px]'>
-      <span className='text-2xl font-semibold'>{title}</span>
-      <div className='flex h-full min-h-0 w-full items-center justify-center'>
+    <div
+      ref={containerRef}
+      className='flex h-full w-[45%] flex-col items-center justify-between gap-5'
+    >
+      <span className='text-xl font-semibold'>{title}</span>
+
+      <div className='flex h-full min-h-3 w-full items-center justify-center'>
         <div className='flex aspect-square h-[min(100%,250px)] w-[min(100%,250px)] items-center justify-center'>
           <PieChart categories={filteredCategories} />
         </div>
       </div>
 
-      <ul className='flex h-full w-full flex-col items-center text-xl'>
+      <ul
+        className={twMerge(
+          'flex h-full w-full flex-col items-center',
+          visibleCount === 0 && 'h-auto'
+        )}
+      >
         {filteredCategories
           .sort((a, b) => b.cost - a.cost)
-          .slice(0, 5)
+          .slice(0, visibleCount)
           .map((category, index) => (
             <li
               key={`${category.category}-${index}`}
               className='flex w-full justify-between font-normal text-caption'
             >
-              <span>{category.category}</span>
+              <span className='flex items-center gap-1 text-main'>
+                <div
+                  className={twMerge(
+                    'flex h-5 w-5 items-center justify-center rounded-full bg-caption text-xs text-white',
+                    getCategoryClass(category.category)
+                  )}
+                >
+                  {index + 1}
+                </div>
+                {category.category}
+              </span>
+
               <span>
                 {isIncome ? '+' : '-'} {category.cost.toLocaleString()}원
               </span>
             </li>
           ))}
 
-        <div className='mt-auto w-full pt-[25px]'>
-          <li className='flex w-full justify-between font-medium'>
+        <div
+          className={twMerge(
+            'mt-auto w-full pt-4',
+            visibleCount === 0 && 'mt-0 pt-0'
+          )}
+        >
+          <li className='flex w-full justify-between text-lg font-medium'>
             <span>합계</span>
             <span>
               {isIncome ? '+' : '-'} {total.toLocaleString()}원
